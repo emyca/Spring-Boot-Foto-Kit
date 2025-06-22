27e2ae6d2fc8511ad7954b8a73893f02f0119a8f
emyca <evmyca@gmail.com>
2025-06-22 16:21:21 +0300

    Update app entry point

--- src/main/java/com/example/Spring_Boot_Foto_Kit/FotoKitApplication.java
+++ src/main/java/com/example/Spring_Boot_Foto_Kit/FotoKitApplication.java
@@ -2,12 +2,19 @@ package com.example.Spring_Boot_Foto_Kit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FotoKitApplication {
+public class FotoKitApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FotoKitApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(FotoKitApplication.class);
 	}
 
 }
