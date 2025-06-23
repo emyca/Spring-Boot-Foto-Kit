73eb36f33d1fc48beb5c366fdd6fbb9472cb26c7
emyca <evmyca@gmail.com>
2025-06-23 10:11:44 +0300

    Add item read all backend logic

---
+++ src/main/java/com/example/Spring_Boot_Foto_Kit/controller/FotoAdminController.java
@@ -0,0 +1,37 @@
+package com.example.Spring_Boot_Foto_Kit.controller;
+
+import com.example.Spring_Boot_Foto_Kit.data.FotoMessage;
+import com.example.Spring_Boot_Foto_Kit.entity.Foto;
+import com.example.Spring_Boot_Foto_Kit.service.admin.FotoAdminService;
+import com.example.Spring_Boot_Foto_Kit.utils.URLS;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/admin")
+public class FotoAdminController {
+
+    @Autowired
+    FotoAdminService service;
+
+    @GetMapping("/fotos")
+    public String getAll(Model model) {
+        List<Foto> list = service.getAll();
+        model.addAttribute("title", "Fotos");
+        if (list.isEmpty()) {
+            model.addAttribute("fotoInfo",
+                    FotoMessage.FAILURE_GET_LIST_MSG.getMessage());
+        } else {
+            model.addAttribute("uploads",
+                    URLS.GET_UPLOADS_URL.getUrl());
+            model.addAttribute("fotos", list);
+        }
+        model.addAttribute("fragmentName", "content");
+        return "admin/@layout";
+    }
+
+}
--- src/main/java/com/example/Spring_Boot_Foto_Kit/service/admin/FotoAdminServiceImpl.java
+++ src/main/java/com/example/Spring_Boot_Foto_Kit/service/admin/FotoAdminServiceImpl.java
@@ -1,24 +1,34 @@
 package com.example.Spring_Boot_Foto_Kit.service.admin;
 
+import com.example.Spring_Boot_Foto_Kit.data.FotoData;
 import com.example.Spring_Boot_Foto_Kit.data.FotoRequest;
 import com.example.Spring_Boot_Foto_Kit.data.FotoResponse;
 import com.example.Spring_Boot_Foto_Kit.entity.Foto;
+import com.example.Spring_Boot_Foto_Kit.repository.FotoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 public class FotoAdminServiceImpl implements FotoAdminService {
 
+    @Autowired
+    FotoRepository repository;
+
+    @Override
+    @Transactional
+    public List<Foto> getAll() {
+        return new FotoData()
+                .toList(repository.findAll());
+    }
+
     @Override
     public FotoResponse add(FotoRequest request) {
         return null;
     }
 
-    @Override
-    public List<Foto> getAll() {
-        return List.of();
-    }
 
     @Override
     public FotoResponse update(Long id, FotoRequest request) {
