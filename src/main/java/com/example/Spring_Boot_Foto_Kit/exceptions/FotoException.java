4bca30458c4b42d7d08703ec9422ce296a8915ec
emyca <evmyca@gmail.com>
2025-06-23 07:21:26 +0300

    Add item exception

---
+++ src/main/java/com/example/Spring_Boot_Foto_Kit/exceptions/FotoException.java
@@ -0,0 +1,7 @@
+package com.example.Spring_Boot_Foto_Kit.exceptions;
+
+public class FotoException extends RuntimeException {
+    public FotoException(String message) {
+        super(message);
+    }
+}
