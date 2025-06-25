97de127f226cbd17c6bfde8c05b2a14fb40f6a8d
emyca <evmyca@gmail.com>
2025-06-25 09:08:48 +0300

    Update request body model

--- src/main/java/com/example/Spring_Boot_Foto_Kit/data/FotoRequest.java
+++ src/main/java/com/example/Spring_Boot_Foto_Kit/data/FotoRequest.java
@@ -1,20 +1,18 @@
 package com.example.Spring_Boot_Foto_Kit.data;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 
+@Setter
+@Getter
+@NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record FotoRequest(
-        Long id,
-        MultipartFile file,
-        String name,
-        String description
-) {
-    public FotoRequest(
-            MultipartFile file,
-            String name,
-            String description
-    ) {
-        this(null, file, name, description);
-    }
+public class FotoRequest {
+    Long id;
+    MultipartFile file;
+    String name;
+    String description;
 }
