e4ef982953e4a412479114e85e5f2537811a8c4c
emyca <evmyca@gmail.com>
2025-10-13 12:03:57 +0300

    Add doc comments

--- src/main/java/com/example/Spring_Boot_Foto_Kit/service/admin/FotoValidator.java
+++ src/main/java/com/example/Spring_Boot_Foto_Kit/service/admin/FotoValidator.java
@@ -9,6 +9,11 @@ import org.springframework.web.multipart.MultipartFile;
 @Component
 public class FotoValidator {
 
+    /**
+     * Validates image file.
+     *
+     * @param data incoming Foto data
+     * */
     public void validateImage(FotoData data) {
         MultipartFile file = data.getFile();
         if (file == null || file.isEmpty())
@@ -16,6 +21,11 @@ public class FotoValidator {
                     FotoMessage.ADD_FILE_MSG.getMessage());
     }
 
+    /**
+     * Validates image name.
+     *
+     * @param data incoming Foto data
+     * */
     public void validateName(FotoData data) {
         String name = data.getName();
         if (name == null || name.isBlank())
@@ -23,6 +33,11 @@ public class FotoValidator {
                     FotoMessage.ADD_NAME_MSG.getMessage());
     }
 
+    /**
+     * Validates image description.
+     *
+     * @param data incoming Foto data
+     * */
     public void validateDescription(FotoData data) {
         String description = data.getDescription();
         if (description == null || description.isBlank())
