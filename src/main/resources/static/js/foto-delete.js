f985c4bfbe860c1e032677e79e973ea82ba8017f
emyca <evmyca@gmail.com>
2026-02-14 10:13:55 +0200

    Refactor front-end delete logic

--- src/main/resources/static/js/foto-delete.js
+++ src/main/resources/static/js/foto-delete.js
@@ -1,5 +1,11 @@
 $(function() {
 
+    const deleteModalCloseBtn = $('#deleteModalCloseBtn');
+    const deleteDeleteBtn = $('#deleteDeleteBtn');
+    const deleteCancelBtn = $("#deleteCancelBtn");
+    const deleteSpinner = $('#deleteSpinner');
+    const deleteResponse =  $('#deleteResponse');
+
     $('button[name="btnDelete"]').click(function(e) {
         e.preventDefault();
         let parentEl = $(this).closest('.card');
@@ -9,44 +15,46 @@ $(function() {
         $('#deleteName').html(fotoName);
     });
 
-    $('#deleteDeleteBtn').click(function(e) {
+    deleteDeleteBtn.click(function(e) {
         e.preventDefault();
-		$("#deleteCancelBtn").prop("disabled", true);
-		$("#deleteDeleteBtn").prop("disabled", true);
-		$('#deleteSpinner').show();
+        deleteCancelBtn.prop("disabled", true);
+        deleteDeleteBtn.prop("disabled", true);
+        deleteSpinner.show();
 
-		let fotoDeleteId = $('span[id=deleteId]').html();
+        let fotoDeleteId = $('span[id=deleteId]').html();
 
         $.ajax({
             type: 'DELETE',
             url: './fotos/' + fotoDeleteId,
             dataType: 'json',
             contentType: false,
-			processData: false
+            processData: false
         })
         .done(function(response) {
-            if (response.success == false) {
-                output = "<span style='color: #f02d1f; font-size: 16px;'>" + response.message + "</span>";
+            if(response.success == false) {
+                deleteResponse
+                    .css({"color": "#f02d1f", "font-size": "16px"})
+                    .html(response.message);
             } else {
-                output = "<span style='color: #22a131; font-size: 16px;'>" + response.message + "</span>";
+                deleteResponse
+                    .css({"color": "#22a131", "font-size": "16px"})
+                    .html(response.message);
             }
-            $('#deleteSpinner').hide();
-            $('#deleteResponse').html(output);
-            $("#deleteDeleteBtn").prop("disabled", false);
+            deleteSpinner.hide();
+            deleteResponse.html(output);
+            deleteDeleteBtn.prop("disabled", false);
         })
         .fail(function(e) {
-            $('#deleteSpinner').hide();
-            $("#deleteResponse").html(e.responseText);
-            $("#deleteDeleteBtn").prop("disabled", false);
+            deleteSpinner.hide();
+            deleteResponse.html(e.responseText);
+            deleteDeleteBtn.prop("disabled", false);
         });
-
     });
 
-    $('#deleteModalCloseBtn').click(function(e) {
-            $('#deleteId').html('');
-            $('#deleteName').html('');
-            $('#deleteResponse').html('');
-            location.reload();
+    deleteModalCloseBtn.click(function(e) {
+        $('#deleteId').html('');
+        $('#deleteName').html('');
+        deleteResponse.html('');
+        location.reload();
     });
-
 });
